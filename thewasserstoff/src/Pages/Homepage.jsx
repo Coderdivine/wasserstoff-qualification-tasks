ba0b8a81ffdd55c68fa7611e088640ed7dbbbbe3
Chimdi <87563898+Coderdivine@users.noreply.github.com>
2022-11-03 12:59:30 +0100

    Explained homepage structure

--- thewasserstoff/src/Pages/Homepage.jsx
+++ thewasserstoff/src/Pages/Homepage.jsx
@@ -3,13 +3,15 @@ import {AxiosConnect} from '../AxiosConnect';
 function Homepage() {
     const [username,setUsername]=useState("");
     const HandleLogin =async()=>{
+      //This codes send a response to backend for validation 
+      //If user validation is true, user identity will be returned with an enum start with the symbol '@'
         AxiosConnect.post("/user/authenticate-user",{username})
         .then(result=>{
             console.log(result);
-            if(result.data.user == "@new_user"){
-                alert(`Welcome to Feynman Board @${result.data.user_details.username}`)
+            if(result.data.user == "@new_user"){//if enum is new_user, that means a new user was created 
+                alert(`Welcome to Feynman Board @${result.data.user_details.username}`)//greetings 
                 localStorage.setItem("ide",result.data.user_details.ide);
-                window.location = `/posts/${result.data.user_details.ide}`;
+                window.location = `/posts/${result.data.user_details.ide}`;//redirect to post route to view user posts
             }else{
                 localStorage.setItem("ide",result.data.user_details.ide);
                 alert(`Welcome back ${username}`)
