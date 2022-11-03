bea984e4892a3d2f31cc4bb3914d79bfa35ba43e
Chimdi <87563898+Coderdivine@users.noreply.github.com>
2022-11-03 13:20:14 +0100

    Addressed code

--- thewasserstoff/src/Pages/Content.jsx
+++ thewasserstoff/src/Pages/Content.jsx
@@ -5,54 +5,59 @@ import {AxiosConnect} from '../AxiosConnect';
 function Content() {
     const[edit,setEdit]=useState(false);
     const [data,setData] =useState(null);
-    const state_ = ['UNDERSTAND','SOMEWHAT UNDERSTAND','NOT CLEAR','WHAT RUBBISH'];
-    const[item_id,setItem_id]=useState('');
+    const state_ = ['UNDERSTAND','SOMEWHAT UNDERSTAND','NOT CLEAR','WHAT RUBBISH'];//required state for each break points
+    const[item_id,setItem_id]=useState('');//holds the recent item id
     const [post_data,setPost_data] = useState(null)
     const {id} = useParams();
     //Post details...
     const [author,setAuthor]=useState("");
     const[title,setTitle]=useState("");
     const[post,setPost]=useState([]);
-    const[text_area,setText_area]=useState("");
+    const[text_area,setText_area]=useState("");hold the post content 
     const getDetails=()=>{
         if( id == "new"){
-            setEdit(true);
-            return "new"
+            //check if id passed is equal to new
+            setEdit(true);//set edit content to true
+            return "new" return some thing random(Not required)
         }else{
         AxiosConnect.post('/blog/a-blog',{id})
         .then(response=>{
-            setEdit(false);
-            const _data = response.data.posts;
+             //This function sends query the backend for a particular Blog content 
+            setEdit(false);//set edit to false. This means we can evaluate the content given
+            const _data = response.data.posts;//returned  value
             console.log(_data);
-             setData(_data);
-             setPost_data(JSON.parse(_data.post));
+             setData(_data);//store data to data variable
+             setPost_data(JSON.parse(_data.post)); store content to postdata variable
         }).catch(err=>{
             console.log(err.message);
             setEdit(false);
-            const msg = err.response.data.message;
+            const msg = err.response.data.message;//Get error message 
             console.log(msg);
             alert(msg);
         })
     }
     }
     useEffect(()=>{
-        getDetails();
+        getDetails();//run function on page reload
     },[])
     const ChangeLevel=(score)=>{
+         //This function changes the recent id state.
         console.log({score});
-        const data_to_evaluate = {blog_id:id,item_id,score:Number(score)};
+        const data_to_evaluate = {blog_id:id,item_id,score:Number(score)};//This data will be send to the backend for evaluation 
         AxiosConnect.post('/blog/evaluate-post',data_to_evaluate)
         .then(result=>{
+            //The result value will be returned. 
             alert("Post Updated");
             console.log(result.data);
         }).catch(err=>{
-            console.log(err);
+            console.log(err);//else show error
         })
     }
     const getId =(id)=>{
         setItem_id(id);
     }
     const ConvertToPost=async(arr)=>{
+       //this function converts text to json by specifying the break point...
         for(let i = 0;i<arr.length;i++ ){
             const obj = {score:4,color:"green",text:arr[i],item_id:i};
             setPost(posts_ => [...posts_, obj]);
@@ -63,6 +68,7 @@ function Content() {
     const Submit=async(e)=>{
         e.preventDefault();
         setText('Posting ...');
+         //the code below specify the required regular expressions to split 
         const regex_ = /[.?;:\n,[)(}{\/-]+/
         const split_to_task = text_area.split(regex_);
         const post_ = await ConvertToPost(split_to_task);
